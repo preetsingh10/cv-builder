9fa46505d131eb3bf4c4a0dbd5dc08c3788fa5ba
Preet Singh <preet10syd@gmail.com>
2025-07-13 11:02:31 +1000

    feat: delete button is working now

--- src/components/editComponents/EditEducation.jsx
+++ src/components/editComponents/EditEducation.jsx
@@ -32,7 +32,14 @@ function EditEducation({ data, setData, currentState }) {
             }
         ))
     }
-
+    function handleDelete() {
+        let newState = currentState.filter(item => {
+            if (item.id !== data.id) {
+                return item
+            }
+        })
+        setData([...newState])
+    }
     return (
         <>
             <div className="education-form edit" >
@@ -43,8 +50,8 @@ function EditEducation({ data, setData, currentState }) {
                 <label htmlFor="edit-titleOfStudy">Title of Study</label>
                 <input type="text" name="titleOfStudy" id="edit-titleOfStudy" value={tempForm.titleOfStudy} onChange={handleChange} />
                 <div className="editButtonContainer">
-                <button className="editButton" type="button" onClick={editFormData}>Apply Changes</button>
-                <button className="deleteButton"><svg xmlns="http://www.w3.org/2000/svg"viewBox="0 -960 960 960"  fill="#EA3323"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" /></svg></button>
+                    <button className="editButton" type="button" onClick={editFormData}>Apply Changes</button>
+                    <button className="deleteButton" onClick={handleDelete}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#EA3323"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" /></svg></button>
                 </div>
             </div>
 
